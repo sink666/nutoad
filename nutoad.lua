fd9dc94587faf3af2b2e4123e4961450c2a0affe
sink666 <er.basilisk@gmail.com>
2022-03-24 22:46:54 +0000

    input to command tape to reading commands implemented

--- nutoad.lua
+++ nutoad.lua
@@ -1,31 +1,66 @@
-result = {}
+array = {}
+input = "+++++>>+++++<+++++>-#"
+commands = {}
+pointer = 1
+
+-- the recommended size of a brainfuck array
 for i = 1, 30000 do
-   result[i] = 0
+   array[i] = 0
 end
 
-function increment_a_at_i(str_foo)
-   print(str_foo)
-   print("foo")
+function increment_a_at_i (index)
+   print("increment fired")
 end
 
-function decrement_a_at_i(str_bar)
-   print(str_bar)
-   print("bar")
+function decrement_a_at_i (index)
+   print("decrement fired")
 end
 
-function barf_from_a_at_i(str_gronk)
-   print(str_gronk)
-   print("gronk")
+--string.char
+function barf_from_a_at_i (index)
+
+end
+
+function pointer_move_left (pointer)
+   print("move left fired")
+end
+
+function pointer_move_right (pointer)
+   print ("move right fired")
+end
+
+function dump_array_state ()
+   print("::final state::")
+   local buffer = {}
+   for i = 1, 10 do
+      buffer[i] = array[i]
+   end
+
+   print(unpack(buffer))
 end
 
 builtin_dictionary = {
-   {id = "+", func = increment_a_at_i},
-   {id = "-", func = decrement_a_at_i},
-   {id = ".", func = barf_from_a_at_i},
+   ["#"] = { func = dump_array_state },
+   ["+"] = { func = increment_a_at_i },
+   ["-"] = { func = decrement_a_at_i },
+   ["."] = { func = barf_from_a_at_i },
+   ["<"] = { func = pointer_move_left },
+   [">"] = { func = pointer_move_right },
 }
 
-for i=1, #builtin_dictionary do
-   print(builtin_dictionary[i].id)
-   builtin_dictionary[i].func("buzz")
+-- extract the commands
+-- in lua you can't just index into a string like in C
+-- so we return a substring at the position (i, i) in the string
+for i = 1, #input do
+   commands[i] = input:sub(i, i)
 end
+
+-- for the length of the commands array, address the dictionary by
+-- the character i in commands and call its function
+
+for i = 1, #commands do
+   builtin_dictionary[commands[i]].func()
+end
+
+-- builtin_dictionary[i].func("test")
 
